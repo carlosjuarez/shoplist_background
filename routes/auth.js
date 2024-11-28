e38db8061a4046c04e8db4e5dae222a53d9f689b
juvcarl <juvcarl@gmail.com>
2024-11-27 21:49:59 -0500

    add log to login

--- routes/auth.js
+++ routes/auth.js
@@ -6,15 +6,15 @@ const jwt = require('jsonwebtoken');
 const authenticateToken = require('../middleware/auth');
 
 router.post('/register', async (req, res) => {
-    const {username,password} = req.body;
+    const { username, password } = req.body;
 
-    try{        
+    try {
         const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({username, password: hashedPassword});
+        const newUser = new User({ username, password: hashedPassword });
         await newUser.save();
         res.status(201).json(newUser);
     } catch (error) {
-        if(error.code === 11000){
+        if (error.code === 11000) {
             res.status(400).send("Username already exists");
         } else {
             res.status(500).send('Error registering user');
@@ -22,52 +22,53 @@ router.post('/register', async (req, res) => {
     }
 });
 
-router.post('/login', async (req,res) => {
-    const {username,password} = req.body;
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
 
-    try{
-        const user = await User.findOne({username});
-        if(!user){
+    try {
+        const user = await User.findOne({ username });
+        if (!user) {
             return res.status(401).send('Invalid username or password');
         }
-        
+
         const isPasswordValid = await bcrypt.compare(password, user.password);
-        if(!isPasswordValid){
+        if (!isPasswordValid) {
             return res.status(402).send('Invalid Username or password');
         }
 
-        const accessToken = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1h'});
-        const refreshToken = jwt.sign({ username }, process.env.REFRESH_SECRET, { expiresIn: '7d'});
+        const accessToken = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const refreshToken = jwt.sign({ username }, process.env.REFRESH_SECRET, { expiresIn: '7d' });
 
         user.refreshToken = refreshToken;
         await user.save();
 
-        res.status(200).json({accessToken, refreshToken});
-    } catch (error){
+        res.status(200).json({ accessToken, refreshToken });
+    } catch (error) {
+        console.log(error)
         res.status(500).send('Error logging in');
     }
 });
 
-router.post('/refresh', async (req,res) =>{
+router.post('/refresh', async (req, res) => {
     const { refreshToken } = req.body;
 
-    if(!refreshToken) return res.status(401).send('Refresh token required');
+    if (!refreshToken) return res.status(401).send('Refresh token required');
 
-    const user = await User.findOne({refreshToken});
-    if(!user) return res.status(403).send('Invalid refresh token');
+    const user = await User.findOne({ refreshToken });
+    if (!user) return res.status(403).send('Invalid refresh token');
 
-    jwt.verify(refreshToken, process.env.REFRESH_SECRET, (err,decoded) => {
+    jwt.verify(refreshToken, process.env.REFRESH_SECRET, (err, decoded) => {
         if (err) return res.status(403).send('Invalid refresh token');
-        const newAccessToken = jwt.sign({username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h'});
-        res.json({accessToken: newAccessToken});
+        const newAccessToken = jwt.sign({ username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        res.json({ accessToken: newAccessToken });
     });
 });
 
-router.post('/logout', async (req,res) => {
-    const {refreshToken} = req.body;
+router.post('/logout', async (req, res) => {
+    const { refreshToken } = req.body;
 
-    const user = await User.findOne({refreshToken});
-    if(!user) return res.status(403).send('Invalid refresh token');
+    const user = await User.findOne({ refreshToken });
+    if (!user) return res.status(403).send('Invalid refresh token');
 
     user.refreshToken = null;
     await user.save();
@@ -75,20 +76,20 @@ router.post('/logout', async (req,res) => {
     res.status(200).send('Logged out');
 });
 
-router.delete('/delete', authenticateToken, async (req,res) => {
+router.delete('/delete', authenticateToken, async (req, res) => {
     const { password } = req.body
 
-    try{
+    try {
         const isPasswordValid = await bcrypt.compare(password, req.user.password);
-        if(!isPasswordValid){
+        if (!isPasswordValid) {
             return res.status(401).send('Invalid username or password');
         }
 
-        await User.deleteOne({username: req.user.username });
+        await User.deleteOne({ username: req.user.username });
 
         res.status(200).send('User account deleted successfully');
 
-    } catch (error){
+    } catch (error) {
         res.status(500).send('Error deleting user account');
     }
 });
